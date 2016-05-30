14f1147343f08010abd49b62d171e9e479707441
Jason.Zhang <lefttjs@gmail.com>
2016-05-30 19:35:55 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/',function(req,res,next) {
+	console.log("req", req)
 	console.log("req.body", req.body)
 	console.log("req.query", req.query)
 	console.log("req.params", req.params)
