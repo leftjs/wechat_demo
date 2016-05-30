04057f1a3fe20a53f6db26c159140fd036d6fa9f
Jason.Zhang <lefttjs@gmail.com>
2016-05-30 20:42:59 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 	console.log("req.body", req.body)
 	console.log("req.query", req.query)
 	console.log("req.params", req.params)
-  res.send(req.query["echostr"])
+  res.send("我是乖宝宝,你说过要收下我的~~~")
 });
 
 router.post('/',function(req,res,next) {
